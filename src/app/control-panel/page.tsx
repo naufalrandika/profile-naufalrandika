a28d889f59657b51c462cbe4808c3d70ce6b3582
Naufal Randika <naufalrandika44@gmail.com>
2024-08-10 02:54:29 +0700

    ga tau

--- src/app/control-panel/page.tsx
+++ src/app/control-panel/page.tsx
@@ -52,12 +52,25 @@ export default function Home() {
             <div className="lg:p-8 items-center justify-center">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <div className="flex flex-col space-y-2 text-center">
+                        <div className="my-5">
                         You are not logged in.
+                        <br />
+                        Only Randi can access control panel.
+                        </div>
+                        If you are Randi, please login.
                         <Link href="/login"
                             className={cn(
                                 buttonVariants({ variant: "ghost" }),
-                                ""
+                                "border border-red-800"
                             )}> Login
+                        </Link>
+                        <br />
+                        but if you are not Randi, please go to home page and have fun!
+                        <Link href="/"
+                            className={cn(
+                                buttonVariants({ variant: "ghost" }),
+                                "border border-red-800"
+                            )}> Home Page
                         </Link>
                     </div>
                 </div>
