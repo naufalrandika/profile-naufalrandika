ba22bd3fcbc5ffc32e54cfd48004639c6d16da26
Naufal Randika <naufalrandika44@gmail.com>
2024-08-10 09:05:01 +0700

    fixed

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,13 +1,7 @@
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
-import { connectDB } from '@/lib/db';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    connectDB();
-  }, []);
-
   return (
     <ThemeProvider>
       <Component {...pageProps} />
--- src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-// lib/db.ts
-import { createClient } from '@vercel/postgres';
-
-const client = createClient({
-  connectionString: process.env.POSTGRES_URL, // Pastikan ini sesuai dengan variabel lingkungan di Vercel
-});
-
-export default client;
